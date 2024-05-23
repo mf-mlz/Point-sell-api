e6cc8219a84d9cb9b52b41671d049af4926b2fdf
“Fernanda <maryfermtzlb@gmail.com>
2024-05-23 17:55:21 -0600

    fix(Clients): Remove route example

--- modules/clients/routes/clientRoutes.js
+++ modules/clients/routes/clientRoutes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const clientController = require('../controllers/clientsController');
 const { verifyToken } = require('../../../middleware/authMiddleware');
 
-
-router.get('/example', clientController.getAllClients);
 router.get('/', verifyToken, clientController.getAllClients);
 
 module.exports = router;
