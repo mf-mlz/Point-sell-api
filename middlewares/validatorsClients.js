2bafe35a8c3f3156d4873850eb849415beaac63e
“Fernanda <maryfermtzlb@gmail.com>
2024-05-30 17:42:46 -0600

    fix(ValidatorsClients): Change error return => Simple Message

--- middlewares/validatorsClients.js
+++ middlewares/validatorsClients.js
@@ -3,12 +3,12 @@ const { body, validationResult, check } = require('express-validator');
 const validateID = [
     body('id')
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo id es requerido')
-        .isNumeric().withMessage('El campo id debe contener solo caracteres numericos'),
+        .isLength({ min: 1 })
+        .isNumeric(),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors });
+            return res.status(400).json({ errors: "Ocurrió un error en la validación de los datos de entrada." });
         }
         next();
     }
@@ -17,37 +17,37 @@ const validateID = [
 const validateClient = [
     body('name')
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo name es requerido')
-        .isString().withMessage('El campo name debe contener solo letras'),
+        .isLength({ min: 1 })
+        .isString(),
     body('email')
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo email es requerido')
-        .isEmail().withMessage('El campo email debe ser un correo electrónico válido'),
+        .isLength({ min: 1 })
+        .isEmail(),
     body('phone')
         .trim()
-        .isLength({ min: 1, max: 10 }).withMessage('El campo phone es requerido en su formato solicitado')
-        .isNumeric().withMessage('El campo phone debe ser numérico'),
+        .isLength({ min: 1, max: 10 })
+        .isNumeric(),
     body('address')
         .trim()
-        .isLength({ min: 1, max: 100 }).withMessage('El campo address es requerido en su formato solicitado')
-        .matches(/^[a-zA-ZÀ-ÿ0-9\s,.'#-]*$/).withMessage('El campo address debe contener solo letras, números y los caracteres especiales permitidos (,.\'-#)'),
+        .isLength({ min: 1, max: 100 })
+        .matches(/^[a-zA-ZÀ-ÿ0-9\s,.'#-]*$/),
     body('zip')
         .trim()
-        .isLength({ min: 1, max: 8 }).withMessage('El campo zip es requerido en su formato solicitado')
-        .isNumeric().withMessage('El campo zip debe contener solo números'),
+        .isLength({ min: 1, max: 8 })
+        .isNumeric(),
     body('tax_system')
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo tax_system es requerido')
-        .isNumeric().withMessage('El campo tax_system debe contener solo números'),
+        .isLength({ min: 1 })
+        .isNumeric(),
     body('tax_id')
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo tax_id es requerido')
-        .matches(/^[A-ZÑ&]{3,4}\d{6}[A-Z0-9]{3}$/).withMessage('El campo tax_id debe ser un RFC válido'),
+        .isLength({ min: 1 })
+        .matches(/^[A-ZÑ&]{3,4}\d{6}[A-Z0-9]{3}$/),
 
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors });
+            return res.status(400).json({ errors: "Ocurrió un error en la validación de los datos de entrada." });
         }
         next();
     }
@@ -56,55 +56,55 @@ const validateClient = [
 const validateClientFilter = [
     check().custom((value, { req }) => {
         if (typeof req.body !== 'object' || req.body === null) {
-            throw new Error('El cuerpo de la solicitud debe ser un objeto');
+            return res.status(400).json({ errors: "El cuerpo de la Solicitud tiene un formato incorrecto." });
         }
         return true;
     }),
     body('id')
         .optional()
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo id es requerido')
-        .isNumeric().withMessage('El campo id debe contener solo caracteres numericos'),
+        .isLength({ min: 1 })
+        .isNumeric(),
     body('name')
         .optional()
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo name es requerido')
-        .isString().withMessage('El campo name debe contener solo letras'),
+        .isLength({ min: 1 })
+        .isString(),
     body('email')
         .optional()
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo email es requerido')
-        .isEmail().withMessage('El campo email debe ser un correo electrónico válido'),
+        .isLength({ min: 1 })
+        .isEmail(),
     body('phone')
         .optional()
         .trim()
-        .isLength({ min: 1, max: 10 }).withMessage('El campo phone es requerido en su formato solicitado')
-        .isNumeric().withMessage('El campo phone debe ser numérico'),
+        .isLength({ min: 1, max: 10 })
+        .isNumeric(),
     body('address')
         .optional()
         .trim()
-        .isLength({ min: 1, max: 100 }).withMessage('El campo address es requerido en su formato solicitado')
-        .matches(/^[a-zA-ZÀ-ÿ0-9\s,.'#-]*$/).withMessage('El campo address debe contener solo letras, números y los caracteres especiales permitidos (,.\'-#)'),
+        .isLength({ min: 1, max: 100 })
+        .matches(/^[a-zA-ZÀ-ÿ0-9\s,.'#-]*$/),
     body('zip')
         .optional()
         .trim()
-        .isLength({ min: 1, max: 8 }).withMessage('El campo zip es requerido en su formato solicitado')
-        .isNumeric().withMessage('El campo zip debe contener solo números'),
+        .isLength({ min: 1, max: 8 })
+        .isNumeric(),
     body('tax_system')
         .optional()
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo tax_system es requerido')
-        .isNumeric().withMessage('El campo tax_system debe contener solo números'),
+        .isLength({ min: 1 })
+        .isNumeric(),
     body('tax_id')
         .optional()
         .trim()
-        .isLength({ min: 1 }).withMessage('El campo tax_id es requerido')
-        .matches(/^[A-ZÑ&]{3,4}\d{6}[A-Z0-9]{3}$/).withMessage('El campo tax_id debe ser un RFC válido'),
+        .isLength({ min: 1 })
+        .matches(/^[A-ZÑ&]{3,4}\d{6}[A-Z0-9]{3}$/),
 
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors });
+            return res.status(400).json({ errors: "Ocurrió un error en la validación de los datos de entrada." });
         }
         next();
     }
