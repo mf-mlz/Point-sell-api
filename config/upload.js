5105adcd63e9a8ccb6f61c695a9db8f1ed117f20
RouterUIZ007 <router.uiz.360@gmail.com>
2024-05-31 13:03:35 -0600

    feat(upload): Add filter type file and size

--- config/upload.js
+++ config/upload.js
@@ -9,14 +9,36 @@ if (!fs.existsSync(uploadDir)) {
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads'); 
+        cb(null, 'uploads');
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
     }
 });
 
-const upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+
+    const filetypes = /jpeg|jpg|png|gif/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        return cb(new Error('Error al Cargar el Archivo'), false);
+
+    }
+
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 1024 * 1024 * 5 
+    }
+});
+
 
 module.exports = {
     upload
--- modules/products/routes/productsRoutes.js
+++ modules/products/routes/productsRoutes.js
@@ -1,15 +1,41 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 const { verifyToken } = require('../../../middlewares/authMiddleware');
 const { verifyAdminRole } = require('../../../middlewares/adminMiddleware');
 const { upload } = require('../../../config/upload');
 
+const handleFileUpload = async (req, res, next) => {
+    try {
+        await new Promise((resolve, reject) => {
+            upload.single('photo')(req, res, (err) => {
+                if (err) {
+                    if (err instanceof multer.MulterError) {
+                        return reject(new Error(`Error al Cargar el Archivo`));
+                    } else {
+                        return reject(new Error(`Error al Cargar el Archivo`));
+                    }
+                }
+                resolve();
+            });
+        });
+        next();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
 router.get('/', verifyToken, productsController.getAllProducts);
 router.get('/filter', verifyToken, productsController.filterProducts);
 
 router.post('/register', verifyToken, verifyAdminRole, productsController.registerProducts);
-router.post('/upload', upload.single('photo'), verifyToken, productsController.uploadPhoto);
+router.post('/upload', handleFileUpload, verifyToken, (req, res, next) => {
+    if (!req.file) {
+        return res.status(400).json({ error: 'El campo photo es requerido' });
+    }
+    productsController.uploadPhoto(req, res, next);
+});
 
 router.put('/edit', verifyToken, verifyAdminRole, productsController.putProducts);
 
