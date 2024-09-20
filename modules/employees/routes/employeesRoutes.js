f80bc79766565969261ef204c8ff50dace7cc2b0
RouterUIZ007 <router.uiz.360@gmail.com>
2024-09-20 13:53:24 -0600

    feat(validatorsEmployees): Add validateEmployeeFilterAll

--- modules/employees/routes/employeesRoutes.js
+++ modules/employees/routes/employeesRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const employeesController = require('../controllers/employeesController');
 const { verifyToken } = require('../../../middlewares/authMiddleware');
 const { verifyAdminRole } = require('../../../middlewares/adminMiddleware');
-const { validateEmployee, validateEmployeeFilter, validateID } = require('../../../middlewares/validatorsEmployees');
+const { validateEmployee, validateEmployeeFilter, validateID, validateEmployeeFilterAll } = require('../../../middlewares/validatorsEmployees');
 
 
 router.get('/', verifyToken, employeesController.getAllEmployees);
@@ -11,6 +11,7 @@ router.get('/', verifyToken, employeesController.getAllEmployees);
 router.post('/register', verifyToken, verifyAdminRole, validateEmployee, employeesController.registerEmployees);
 router.post('/login', employeesController.loginEmployees);
 router.post('/filter', verifyToken, validateEmployeeFilter, employeesController.filterEmployees);
+router.post('/filterAll', verifyToken, validateEmployeeFilterAll, employeesController.filterEmployeesAll);
 router.put('/edit', verifyToken, verifyAdminRole, validateID, validateEmployee, employeesController.putEmployees);
 router.delete('/delete', verifyToken, verifyAdminRole, validateID, employeesController.deleteEmployee);
 
