edd82c4d20eb68afc8a5602c9e0d3849b20e6e90
“Fernanda <maryfermtzlb@gmail.com>
2024-09-30 16:53:51 -0600

    fix(Employees Route): Fix Validation verify

--- modules/employees/routes/employeesRoutes.js
+++ modules/employees/routes/employeesRoutes.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 const employeesController = require('../controllers/employeesController');
 const { verifyToken } = require('../../../middlewares/authMiddleware');
-const { verifyAdminRole } = require('../../../middlewares/adminMiddleware');
+const { verifyRootUser } = require('../../../middlewares/adminMiddleware');
 const { validateEmployee, validateEmployeeFilter, validateID, validateEmployeeFilterAll } = require('../../../middlewares/validatorsEmployees');
 
 
 router.get('/', verifyToken, employeesController.getAllEmployees);
 
-router.post('/register', verifyToken, verifyAdminRole, validateEmployee, employeesController.registerEmployees);
+router.post('/register', verifyToken, verifyRootUser, validateEmployee, employeesController.registerEmployees);
 router.post('/login', employeesController.loginEmployees);
 router.post('/filter', verifyToken, validateEmployeeFilter, employeesController.filterEmployees);
 router.post('/filterAll', verifyToken, validateEmployeeFilterAll, employeesController.filterEmployeesAll);
-router.put('/edit', verifyToken, verifyAdminRole, validateID, validateEmployee, employeesController.putEmployees);
-router.delete('/delete', verifyToken, verifyAdminRole, validateID, employeesController.deleteEmployee);
+router.put('/edit', verifyToken, verifyRootUser, validateID, validateEmployee, employeesController.putEmployees);
+router.delete('/delete', verifyToken, verifyRootUser, validateID, employeesController.deleteEmployee);
 
 module.exports = router;
