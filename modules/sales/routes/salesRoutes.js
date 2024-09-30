18791357496f0dc52772c549b20df52bd2dda321
“Fernanda <maryfermtzlb@gmail.com>
2024-09-30 16:57:19 -0600

    feat(Sale): Change name validation name

--- modules/sales/routes/salesRoutes.js
+++ modules/sales/routes/salesRoutes.js
@@ -2,18 +2,19 @@ const express = require('express');
 const router = express.Router();
 const salesController = require('../controllers/salesController');
 const { verifyToken } = require('../../../middlewares/authMiddleware');
-const { verifyAdminRole } = require('../../../middlewares/adminMiddleware');
+const { verifyRootUser, verifyRolSaleRegister } = require('../../../middlewares/adminMiddleware');
 const { validateSale, validateID, validateSaleFilter } = require('../../../middlewares/validatorsSale');
+const { processPayment } = require('../../../services/openpay');
 
 
 router.get('/', verifyToken, salesController.getAllSales);
 
-router.post('/register', verifyToken, verifyAdminRole, salesController.registerSales);
+router.post('/register', verifyToken, validateSale, verifyRolSaleRegister, salesController.registerSales);
+router.post('/registerPayment', verifyToken, validateSale, verifyRolSaleRegister, salesController.registerSales);
 router.post('/filter', verifyToken, validateSaleFilter, salesController.filterSales);
 router.post('/getSaleDate', verifyToken, validateSaleFilter, salesController.getSaleDate);
-
-router.put('/edit', verifyToken, validateID, validateSale, verifyAdminRole, salesController.putSale);
-
-router.delete('/delete', verifyToken, validateID, verifyAdminRole, salesController.deleteSale);
+router.put('/edit', verifyToken, validateID, validateSale, verifyRootUser, salesController.putSale);
+router.delete('/delete', verifyToken, validateID, verifyRootUser, salesController.deleteSale);
+router.post('/payment', verifyToken, verifyRolSaleRegister, processPayment);
 
 module.exports = router;
