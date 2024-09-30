6353f53206beab6735d26c402b7874165a3bfa51
“Fernanda <maryfermtzlb@gmail.com>
2024-09-30 16:55:00 -0600

    feat(Clients): Change name validation name

--- modules/clients/routes/clientRoutes.js
+++ modules/clients/routes/clientRoutes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientsController');
 const { verifyToken } = require('../../../middlewares/authMiddleware');
-const { verifyAdminRole } = require('../../../middlewares/adminMiddleware');
+const { verifyRootUser } = require('../../../middlewares/adminMiddleware');
 const { validateClient, validateID, validateClientFilter } = require('../../../middlewares/validatorsClients');
 
 router.get('/', verifyToken, clientController.getAllClients);
 
-router.post('/register', verifyToken, verifyAdminRole, validateClient, clientController.registerClients);
+router.post('/register', verifyToken, verifyRootUser, validateClient, clientController.registerClients);
 router.get('/filter', verifyToken, validateClientFilter, clientController.filterClients);
-router.put('/edit', verifyToken, validateID, validateClient, verifyAdminRole, clientController.putClients);
-router.delete('/delete', verifyToken, validateID, verifyAdminRole, clientController.deleteClient);
+router.put('/edit', verifyToken, validateID, validateClient, verifyRootUser, clientController.putClients);
+router.delete('/delete', verifyToken, validateID, verifyRootUser, clientController.deleteClient);
 
 module.exports = router;
