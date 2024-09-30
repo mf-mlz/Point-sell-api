e616475048469a69a56405048e231bd72c372b26
“Fernanda <maryfermtzlb@gmail.com>
2024-09-30 16:56:49 -0600

    feat(Sale): Add new Process => Insert Sale and Update Sale (Payment by money and card)

--- modules/sales/controllers/salesController.js
+++ modules/sales/controllers/salesController.js
@@ -1,131 +1,158 @@
-const salesService = require('../services/salesService');
-const { verifyData, createUpdatetAt } = require('../../../utils/helpers');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const salesService = require("../services/salesService");
+const { verifyData, createUpdatetAt } = require("../../../utils/helpers");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const registerSales = async (req, res) => {
+  const requiredFields = [
+    "date",
+    "payment",
+    "dataPayment",
+    "employeesId",
+    "status",
+  ];
+  const data = req.body;
 
-    const requiredFields = ['date', 'totalAmount', 'payment', 'dataPayment', 'customerId', 'employeesId', 'status'];
-    const data = req.body;
+  const missingField = verifyData(requiredFields, data);
+  if (missingField) {
+    return res
+      .status(400)
+      .json({ error: `El campo ${missingField} es requerido` });
+  }
 
-    const missingField = verifyData(requiredFields, data);
-    if (missingField) {
-        return res.status(400).json({ error: `El campo ${missingField} es requerido` });
+  try {
+    const registerSalesService = await salesService.registerSales(data);
+    if (registerSalesService > 0) {
+      res
+        .status(200)
+        .json({ message: "Venta Registrada con Éxito y Stock Actualizado", idSale: registerSalesService  });
     }
-
-    const { date, totalAmount, payment, dataPayment, customerId, employeesId, status } = data;
-
-    try {
-
-        const registerSalesService = await salesService.registerSales(data);
-        res.status(201).json({ message: registerSalesService });
-
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 const filterSales = async (req, res) => {
-
-    const data = req.body;
-    try {
-        delete data.employeeId;
-        const salesData = await salesService.getSale(data);
-        if (salesData.length > 0) {
-            res.status(401).json({ message: `Se encontraron ${salesData.length} registros`, sales: salesData });
-
-        } else {
-            res.status(401).json({ message: `No se encontraron registros` });
-        }
-
-    } catch (err) {
-        res.status(500).json({ error: 'Ocurrió un error al obtener los registros' });
+  const data = req.body;
+  try {
+    delete data.employeeId;
+    const salesData = await salesService.getSale(data);
+    if (salesData.length > 0) {
+      res
+        .status(401)
+        .json({
+          message: `Se encontraron ${salesData.length} registros`,
+          sales: salesData,
+        });
+    } else {
+      res.status(401).json({ message: `No se encontraron registros` });
     }
-
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al obtener los registros" });
+  }
 };
 
 const getAllSales = async (req, res) => {
-    try {
-        const sales = await salesService.getAllSales();
-        res.json(sales);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const sales = await salesService.getAllSales();
+    res.json(sales);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const putSale = async (req, res) => {
+  const requiredFields = [
+    "id",
+    "date",
+    "payment",
+    "dataPayment",
+    // "customerId",
+    "employeesId",
+    "status",
+  ];
+  const data = req.body;
 
-    const requiredFields = ['id', 'date', 'payment', 'dataPayment', 'customerId', 'employeesId', 'status'];
-    const data = req.body;
+  const missingField = verifyData(requiredFields, data);
+  if (missingField) {
+    return res
+      .status(400)
+      .json({ error: `El campo ${missingField} es requerido` });
+  }
 
-    const missingField = verifyData(requiredFields, data);
-    if (missingField) {
-        return res.status(400).json({ error: `El campo ${missingField} es requerido` });
-    }
+  const {
+    id,
+    date,
+    totalAmount,
+    payment,
+    dataPayment,
+    customerId,
+    employeesId,
+    status,
+  } = data;
 
-    const { id, date, totalAmount, payment, dataPayment, customerId, employeesId, status } = data;
+  try {
+    data.updated_at = createUpdatetAt();
 
-    try {
-
-        data.updated_at = createUpdatetAt();
-
-        const salesEmployeesServices = await salesService.putSale(data);
-        res.status(201).json({ message: salesEmployeesServices });
-
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+    const salesEmployeesServices = await salesService.putSale(data);
+    res.status(201).json({ message: salesEmployeesServices });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 const deleteSale = async (req, res) => {
+  const requiredFields = ["id"];
+  const data = req.body;
 
-    const requiredFields = ['id'];
-    const data = req.body;
+  const missingField = verifyData(requiredFields, data);
+  if (missingField) {
+    return res
+      .status(400)
+      .json({ error: `El campo ${missingField} es requerido` });
+  }
 
-    const missingField = verifyData(requiredFields, data);
-    if (missingField) {
-        return res.status(400).json({ error: `El campo ${missingField} es requerido` });
-    }
+  const { id } = data;
 
-    const { id } = data;
-
-    try {
-
-        const deleteSaleServices = await salesService.deleteSale(data);
-        res.status(201).json({ message: deleteSaleServices });
-
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const deleteSaleServices = await salesService.deleteSale(data);
+    res.status(201).json({ message: deleteSaleServices });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-
 const getSaleDate = async (req, res) => {
-
-    const data = req.body;
-    try {
-        delete data.employeeId;
-        const getSaleDate = await salesService.getSaleDate(data);
-        if (getSaleDate.length > 0) {
-            res.status(200).json({ message: `Se encontraron ${getSaleDate.length} registros`, sales: getSaleDate });
-
-        } else {
-            res.status(200).json({ message: `No se encontraron registros` });
-        }
-
-    } catch (err) {
-        res.status(500).json({ error: 'Ocurrió un error al obtener los registros' });
+  const data = req.body;
+  try {
+    delete data.employeeId;
+    const getSaleDate = await salesService.getSaleDate(data);
+    if (getSaleDate.length > 0) {
+      res
+        .status(200)
+        .json({
+          message: `Se encontraron ${getSaleDate.length} registros`,
+          sales: getSaleDate,
+        });
+    } else {
+      res.status(200).json({ message: `No se encontraron registros` });
     }
-
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al obtener los registros" });
+  }
 };
 
 module.exports = {
-    registerSales,
-    filterSales,
-    getAllSales,
-    putSale,
-    deleteSale,
-    getSaleDate
+  registerSales,
+  filterSales,
+  getAllSales,
+  putSale,
+  deleteSale,
+  getSaleDate,
 };
