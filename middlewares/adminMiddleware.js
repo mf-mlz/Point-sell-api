dadd70260dc1756f66a1f01d7a602ad5f953272e
“Fernanda <maryfermtzlb@gmail.com>
2024-09-30 16:52:46 -0600

    feat(Admin Middleware): Add verifyRolSaleRegister (Root and Sale)

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const verifyAdminRole = async (req, res, next) => {
+const verifyRootUser = async (req, res, next) => {
     try {
         const idUser = req.body.employeeId || req.query.employeeId || req.params.employeeId || req.headers['employeeid'];
         const data = { "id" : idUser };
@@ -23,7 +23,28 @@ const verifyAdminRole = async (req, res, next) => {
     }
 };
 
+const verifyRolSaleRegister = async (req, res, next) => {
+    try {
+        const idUser = req.body.employeeId || req.query.employeeId || req.params.employeeId || req.headers['employeeid'];
+        const data = { "id" : idUser };
+
+        const employeeData = await employeesService.getEmployee(data);
+        const nameRole = employeeData[0]?.role_name;
+
+        if (nameRole === undefined) {
+            return res.status(401).json({ message: 'Acceso Denegado: El ID del Empleado no es Válido' });
+        } else if (nameRole !== process.env.ROLE_REGISTER || nameRole !== process.env.ROLE_NAME) {
+            return res.status(401).json({ message: 'Acceso Denegado: No cuentas con los permisos necesarios para realizar esta acción.' });
+        }
+
+        next();
+    } catch (err) {
+        res.status(400).json({ message: 'Acceso Inválido' });
+    }
+};
+
 
 module.exports = {
-    verifyAdminRole
+    verifyRootUser,
+    verifyRolSaleRegister
 };
