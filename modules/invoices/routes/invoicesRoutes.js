b57cd9034cbd9a4dceb2cc29537e1bb5479e5bf8
“Fernanda <maryfermtzlb@gmail.com>
2024-09-30 16:55:13 -0600

    feat(Invoices): Change name validation name

--- modules/invoices/routes/invoicesRoutes.js
+++ modules/invoices/routes/invoicesRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const invoicesController = require('../controllers/invoicesController');
 const { verifyToken } = require('../../../middlewares/authMiddleware');
-const { verifyAdminRole } = require('../../../middlewares/adminMiddleware');
-const { validateID, validateInvoice, validateInvoiceFilter, validateIdDownload, validateDataCancel } = require('../../../middlewares/validatorsInvoice');
+const { verifyRootUser, verifyRolSaleRegister } = require('../../../middlewares/adminMiddleware');
+const { validateID, validateInvoice, validateIdDownload, validateDataCancel } = require('../../../middlewares/validatorsInvoice');
 
-router.post('/create', verifyToken, verifyAdminRole, validateInvoice, invoicesController.createInvoice);
-router.post('/sendEmail', verifyToken, verifyAdminRole, invoicesController.sendEmail);
-router.post('/download', verifyToken, verifyAdminRole, validateIdDownload, invoicesController.downloadInvoice);
-router.post('/cancel', verifyToken, verifyAdminRole, validateDataCancel, invoicesController.cancelInvoice);
-router.post('/getByIdSale', verifyToken, verifyAdminRole, validateID, invoicesController.getInvoicesByIdSale);
+router.post('/create', verifyToken, verifyRootUser, validateInvoice, invoicesController.createInvoice);
+router.post('/sendEmail', verifyToken, verifyRootUser, invoicesController.sendEmail);
+router.post('/download', verifyToken, verifyRootUser, validateIdDownload, invoicesController.downloadInvoice);
+router.post('/cancel', verifyToken, verifyRootUser, validateDataCancel, invoicesController.cancelInvoice);
+router.post('/getByIdSale', verifyToken, verifyRolSaleRegister, validateID, invoicesController.getInvoicesByIdSale);
 
 module.exports = router;
