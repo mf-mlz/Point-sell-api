e58eeaab811402a19e876a25f3414090223e26c1
“Fernanda <maryfermtzlb@gmail.com>
2024-10-09 14:30:18 -0600

    fix(Auth): Change Crypto => Crypto-js

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,56 +1,75 @@
-const employeesService = require('../modules/employees/services/employeesService');
-const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
+const employeesService = require("../modules/employees/services/employeesService");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+const { decryptCrypt } = require("../utils/crypto-js");
 
 dotenv.config();
 
 const verifyRootUser = async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        const payload = jwt.decode(token);
-        
-        const data = { "id" : payload.id };
+  try {
+    const token = req.cookies.token;
+    let payload = jwt.decode(token);
+    payload = decryptCrypt(payload.data);
+    const data = { id: payload.id };
 
-        const employeeData = await employeesService.getEmployee(data);
-        const nameRole = employeeData[0]?.role_name;
+    const employeeData = await employeesService.getEmployee(data);
+    const nameRole = employeeData[0]?.role_name;
 
-        if (nameRole === undefined) {
-            return res.status(401).json({ message: 'Acceso Denegado: El ID del Empleado no es Válido' });
-        } else if (nameRole !== process.env.ROLE_NAME) {
-            return res.status(401).json({ message: 'Acceso Denegado: No cuentas con los permisos necesarios para realizar esta acción.' });
-        }
-
-        next();
-    } catch (err) {
-        res.status(400).json({ message: 'Acceso Inválido' });
+    if (nameRole === undefined) {
+      return res
+        .status(401)
+        .json({ message: "Acceso Denegado: El ID del Empleado no es Válido" });
+    } else if (nameRole !== process.env.ROLE_NAME) {
+      return res
+        .status(401)
+        .json({
+          message:
+            "Acceso Denegado: No cuentas con los permisos necesarios para realizar esta acción.",
+        });
     }
+
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Acceso Inválido" });
+  }
 };
 
 const verifyRolSaleRegister = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    let payload = jwt.decode(token);
+    
+    payload = decryptCrypt(payload.data);
+    const data = { id: payload.id };
 
-    try {
-        const token = req.cookies.token;
-        const payload = jwt.decode(token);
+    const employeeData = await employeesService.getEmployee(data);
+    const nameRole = employeeData[0]?.role_name;
 
-        const data = { "id" : payload.id };
-
-        const employeeData = await employeesService.getEmployee(data);
-        const nameRole = employeeData[0]?.role_name;
-
-        if (nameRole === undefined) {
-            return res.status(401).json({ message: 'Acceso Denegado: El ID del Empleado no es Válido' });
-        } else if (nameRole !== process.env.ROLE_REGISTER && nameRole !== process.env.ROLE_NAME) {
-            return res.status(401).json({ message: 'Acceso Denegado: No cuentas con los permisos necesarios para realizar esta acción.' });
-        }
-
-        next();
-    } catch (err) {
-        res.status(400).json({ message: 'Acceso Inválido' });
+    if (nameRole === undefined) {
+      return res
+        .status(401)
+        .json({ message: "Acceso Denegado: El ID del Empleado no es Válido" });
+    } else if (
+      nameRole !== process.env.ROLE_REGISTER &&
+      nameRole !== process.env.ROLE_NAME
+    ) {
+      return res
+        .status(401)
+        .json({
+          message:
+            "Acceso Denegado: No cuentas con los permisos necesarios para realizar esta acción.",
+        });
     }
+
+    next();
+  } catch (err) {
+    console.log(err);
+
+    res.status(400).json({ message: "Acceso Inválido" });
+  }
 };
 
-
 module.exports = {
-    verifyRootUser,
-    verifyRolSaleRegister
+  verifyRootUser,
+  verifyRolSaleRegister,
 };
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,15 +2,17 @@ const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const path = require("path");
 const { deleteFile } = require("../utils/files");
+const { decryptCrypt } = require("../utils/crypto-js");
+
 /* Key ECDSA (ES256) */
 const pubKey = fs.readFileSync(
   path.join(process.cwd(), process.env.KNP),
   "utf8"
 );
 
+/* Verify JWT => Api Peticion */
 const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  
   if (!token) {
     if (req.file) {
       deleteFile(req.file)
@@ -55,11 +57,63 @@ const verifyToken = (req, res, next) => {
         }
       });
     } catch (err) {
+      console.log(err);
+      
       res.status(401).json({ message: "Acceso Inválido" });
     }
   }
 };
 
+/* Verify Token Validation and Header => Guard Routes and Data Session Storage */
+const validationToken = (req, res, next) => {
+  const token = req.cookies.token;
+  let userSessionEncrypt = req.headers["session-employee"];
+  userSessionEncrypt = userSessionEncrypt ? userSessionEncrypt.replace(/['"]+/g, '') : null;
+
+  const payloadEncrypt = jwt.decode(token);
+
+  /* Headers or Token Not Found */
+  if (!userSessionEncrypt || !token) {
+    return res.status(401).json({ status: false, data: {} });
+  } else {
+    try {
+      /* Verify Is Valid Token */
+      jwt.verify(token, pubKey, { algorithms: ["ES256"] }, (err, decoded) => {
+        if (err) {
+          return res.status(401).json({ status: false, data: {} });
+        } else {
+          /* Verify Payload == User Session */
+          /* Desencryp Payload and Session Employee */
+          let payload = decryptCrypt(payloadEncrypt.data);
+          let userSession = decryptCrypt(userSessionEncrypt);
+          
+          if (!validateDecodedToken(userSession, payload)) {
+            return res.status(401).json({ status: false, data: {} });
+          }else{
+            return res.status(200).json({ status: true, data: userSessionEncrypt });
+          }
+        }
+      });
+    } catch (err) {
+      return res.status(401).json({ status: false, data: {} });
+    }
+  }
+};
+
+/* Verify Payload */
+const validateDecodedToken = (userSession, payload) => {
+  if (
+    userSession.id === payload.id &&
+    userSession.name === payload.name &&
+    userSession.role_name === payload.role_name
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 module.exports = {
   verifyToken,
+  validationToken,
 };
